12560239d3f9aee522c122bb6a66b355d2720d63
Prakhar Tiwari <65062036+prakhartiwari0@users.noreply.github.com>
2023-11-22 20:25:32 +0530

    work in progress

--- script.js
+++ script.js
@@ -10,14 +10,25 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Show the scroll indicator after scrolling a certain distance
     window.addEventListener('scroll', function () {
-        if (window.scrollY > 300) {
+        if (window.scrollY > 200) {
             quickAccess.style.display = 'flex';
+
             scrollToTopButton.style.display = 'flex';
         } else {
             quickAccess.style.display = 'none';
             scrollToTopButton.style.display = 'none';
         }
     });
+
+    quickAccess.addEventListener('mouseenter', (e)=>{
+        let quickAccessDetails = document.querySelector('.quickAccess-wrapper details');
+        console.log(quickAccessDetails);
+        quickAccessDetails.open = true
+    })
+    quickAccess.addEventListener('mouseleave', (e)=>{
+        let quickAccessDetails = document.querySelector('.quickAccess-wrapper details');
+        quickAccessDetails.open = false
+    })
 
     // Smooth scroll to top when the scroll indicator is clicked
     scrollToTopButton.addEventListener('click', function () {
@@ -26,4 +37,24 @@ document.addEventListener('DOMContentLoaded', function () {
             behavior: 'smooth'
         });
     });
+
+    document.addEventListener('mousemove', e=>{
+        if (e.x >= 800 && e.y <= 80) {
+            quickAccess.style.display = 'flex'
+            
+        }
+    })
 });
+
+
+const switchModeButton = document.querySelector('.switchMode')
+switchModeButton.addEventListener('click', ()=>{
+    const root = document.querySelector('html')
+    if (root.classList.contains('dark')) {
+        root.classList.remove('dark')
+    }
+    else{
+        root.classList.add('dark')
+    }
+
+})
