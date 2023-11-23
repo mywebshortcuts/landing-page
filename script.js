34ac96dc453cf714f914b35f491b009fb9a5a04a
Prakhar Tiwari <65062036+prakhartiwari0@users.noreply.github.com>
2023-11-23 20:26:30 +0530

    wip

--- script.js
+++ script.js
@@ -29,21 +29,48 @@ document.addEventListener('DOMContentLoaded', function () {
         let quickAccessDetails = document.querySelector('.quickAccess-wrapper details');
         quickAccessDetails.open = false
     })
+    document.addEventListener('mousemove', e=>{
+        if (e.x >= 800 && e.y <= 80) {
+            quickAccess.style.display = 'flex'
+        }
+    })
 
-    // Smooth scroll to top when the scroll indicator is clicked
+    
+    // Scroll to top functionality
     scrollToTopButton.addEventListener('click', function () {
         window.scrollTo({
             top: 0,
+            // Smooth scroll to top when the scroll indicator is clicked
             behavior: 'smooth'
         });
     });
 
-    document.addEventListener('mousemove', e=>{
-        if (e.x >= 800 && e.y <= 80) {
-            quickAccess.style.display = 'flex'
-            
-        }
+
+
+    // Subscribe dialog functionality
+    const subscribeDialog = document.querySelector('.subscribeDialog')
+    subscribeDialog.style.display = 'none'
+    console.log(subscribeDialog);
+
+
+    const chromeInstallLink = document.querySelector('.chromeWebstoreLink')
+    chromeInstallLink.addEventListener('click', (e) => {
+        e.preventDefault()
+        subscribeDialog.showModal()
+        subscribeDialog.style.display = 'flex'
     })
+
+    const closeSubscribeDialogButton = subscribeDialog.querySelector('.closeSubscribeDialogButton')
+    console.log(closeSubscribeDialogButton);
+
+
+    closeSubscribeDialogButton.addEventListener('click', (e) => {
+        subscribeDialog.close()
+        subscribeDialog.style.display = 'none'
+    })
+
+
+
 });
 
 
@@ -57,33 +84,6 @@ switchModeButton.addEventListener('click', ()=>{
         root.classList.add('dark')
     }
 
-})
-
-
-
-
-// const subscribeDialogTemplate = (document.querySelector('.subscribeDialogTemplate')).content.cloneNode(true)
-// console.log(subscribeDialogTemplate);
-const subscribeDialog = document.querySelector('.subscribeDialog')
-// document.body.appendChild(subscribeDialog)
-subscribeDialog.style.display = 'none'
-console.log(subscribeDialog);
-
-
-const chromeInstallLink = document.querySelector('.chromeWebstoreLink')
-chromeInstallLink.addEventListener('click', (e)=>{
-    e.preventDefault()
-    subscribeDialog.showModal()
-    subscribeDialog.style.display = 'flex'
-})
-
-const closeSubscribeDialogButton = subscribeDialog.querySelector('.closeSubscribeDialogButton')
-console.log(closeSubscribeDialogButton);
-
-
-closeSubscribeDialogButton.addEventListener('click', (e)=>{
-    subscribeDialog.close()
-    subscribeDialog.style.display = 'none'
 })
 
 
